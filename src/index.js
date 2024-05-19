baad05e0abb59bc6a5ce762c28b1be765c017408
21501A05A1 <ksowji333@gmail.com>
2024-05-19 12:26:18 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -3,12 +3,29 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import {AuthProvider} from "./store/auth.jsx";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <AuthProvider>
+  <>
+  <App />
+  <ToastContainer
+   position="top-right"
+   autoClose={3000}
+   hideProgressBar={false}
+   newestOnTop={false}
+   closeOnClick
+   rtl={false}
+   pauseOnFocusLoss
+   draggable
+   pauseOnHover
+   theme="colored"
+   bodyClassName="toastBody"
+  />
+  </>
+  </AuthProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
